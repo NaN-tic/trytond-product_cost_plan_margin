85280d4e55e5c981a6b72d656a00491a1c0d1539
Albert Cervera i Areny <albert@nan-tic.com>
2014-03-16 03:45:56 +0100

    Adapt the module to the removal of quantity and total_cost in product_cost_plan and refactor.

--- plan.py
+++ plan.py
@@ -68,48 +68,29 @@ class PlanCost:
 class Plan:
     __name__ = 'product.cost.plan'
 
-    unit_margin = fields.Function(fields.Float('Unit Margin', digits=(14, 4)),
-        'get_margins')
-    percent_margin = fields.Function(fields.Float('Margin %', digits=(14, 4)),
-        'get_margins')
-    unit_price = fields.Function(fields.Numeric('Unit Price'),
-        'get_unit_price')
-    total_margin = fields.Function(fields.Numeric('Total Margin'),
-        'get_margins')
-    total_price = fields.Function(fields.Numeric('Total Price'),
-        'get_total_price')
+    margin = fields.Function(fields.Numeric('Margin', digits=(16, 4),
+            on_change_with=['costs']), 'on_change_with_margin')
+    margin_percent = fields.Function(fields.Numeric('Margin %', digits=(14, 4),
+            on_change_with=['costs', 'products', 'cost_price']),
+        'on_change_with_margin_percent')
+    unit_price = fields.Function(fields.Numeric('Unit Price', digits=(16, 4),
+            on_change_with=['costs', 'products', 'cost_price']),
+        'on_change_with_unit_price')
 
-    def get_unit_price(self, name=None):
-        if not self.total_cost or not self.quantity or not self.unit_margin:
-            return Decimal('0.0')
-        return ((self.total_cost / Decimal(str(self.quantity)))
-            + Decimal(str(self.unit_margin)))
+    def on_change_with_unit_price(self, name=None):
+        unit_price = Decimal('0.0')
+        if self.cost_price:
+            unit_price += self.cost_price
+        margin = self.on_change_with_margin()
+        if margin:
+            unit_price += margin
+        return unit_price
 
-    @classmethod
-    def get_margins(cls, plans, names):
-        res = {}
-        for name in names:
-            res[name] = dict([(p.id, 0.0) for p in plans])
+    def on_change_with_margin(self, name=None):
+        return sum(c.margin for c in self.costs)
 
-        for plan in plans:
-            total_margin = sum(Decimal(str(m.margin)) for m in plan.costs)
-            if 'total_margin' in names:
-                res['total_margin'][plan.id] = total_margin
-
-            if plan.total_cost != _ZERO and 'percent_margin' in names:
-                res['percent_margin'][plan.id] = round(float(total_margin /
-                    plan.total_cost), 4)
-
-            quantity = Decimal(str(plan.quantity))
-            if quantity != _ZERO and 'unit_margin' in names:
-                res['unit_margin'][plan.id] = round(float(total_margin /
-                        quantity), 4)
-        return res
-
-    def get_total_price(self, name):
-        return self.total_cost + self.total_margin
-
-    def get_cost_line(self, cost_type, field_name):
-        res = super(Plan, self).get_cost_line(cost_type, field_name)
-        res['margin_percent'] = cost_type.minimum_percent
-        return res
+    def on_change_with_margin_percent(self, name=None):
+        if self.cost_price == _ZERO:
+            return
+        margin = self.on_change_with_margin()
+        return margin / self.cost_price
